df8a09f372b54b3c2aec2f68c6947b3204de1801
Fernando Paredes <nano@fdp.io>
2017-06-24 22:06:37 -0700

    Ask for name

--- index.js
+++ index.js
@@ -5,12 +5,25 @@ const inquirer = require("inquirer")
 const templatesPath = path.resolve(process.env.HOME, "templates")
 const choices = fs.readdirSync(templatesPath)
 
+const validateName = input => {
+  if (/^([A-Za-z\-\_\d])+$/.test(input)) {
+    return true
+  } else {
+    return 'Project name may only include letters, numbers, underscores and hashes.'
+  }
+}
+
 const QUESTIONS = [
   {
     name: "template",
     type: "list",
     message: "What template would you like to generate?",
     choices
+  },
+  {
+    name: "name",
+    message: "Project Name",
+    validate: validateName
   }
 ]
 
