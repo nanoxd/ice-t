c0f41aec942da4cc1603411bed545309b3801587
Fernando Paredes <nano@fdp.io>
2017-06-24 21:59:56 -0700

    Choose a template

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require("fs")
+const path = require("path")
+const inquirer = require("inquirer")
 
-const templatesPath = path.resolve(process.env.HOME, 'templates')
-const CHOICES = fs.readdirSync(templatesPath)
+const templatesPath = path.resolve(process.env.HOME, "templates")
+const choices = fs.readdirSync(templatesPath)
+
+const QUESTIONS = [
+  {
+    name: "template",
+    type: "list",
+    message: "What template would you like to generate?",
+    choices
+  }
+]
+
+inquirer.prompt(QUESTIONS)
+    .then(console.log)
+    .catch(console.log)
