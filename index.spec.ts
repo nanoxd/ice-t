933d72db20ee0fb1bc3cd714932650672f87dda9
Fernando Paredes <nano@fdp.io>
2017-06-27 09:15:11 -0700

    Add test for validate name

---
+++ index.spec.ts
@@ -0,0 +1,13 @@
+import { validateName } from './templater'
+
+describe('validateName', () => {
+  it('should validate name', () => {
+    const name = 'Aurelius'
+    expect(validateName(name)).toBe(true)
+  })
+
+  it('should return error when a name has spaces', () => {
+    const invalidName = 'A name with spaces'
+    expect(validateName(invalidName)).toBe("Project name may only include letters, numbers, underscores and hashes.")
+  })
+})
